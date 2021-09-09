f4445b7a3ed6005ee18bdf44c14866f4a998c46a
Dmitriy <dmitriyolv@gmail.com>
2021-09-09 23:57:20 +0300

    simple changes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 
     $('.banner-section__slider').slick({
         dots:true,
+        autoplay: true,
+        autoplaySpeed: 4000,
         prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="images/arrow-left.svg" alt=""></button>',
         nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/arrow-right.svg" alt=""></button>',
         responsive: [
